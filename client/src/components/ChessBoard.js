e1aadf0190f918f9c08405eb788fc73609e985b4
Ananya Gupta <ananyag2806@gmail.com>
2022-02-10 00:32:38 +0530

    commit

--- client/src/components/ChessBoard.js
+++ client/src/components/ChessBoard.js
@@ -1,5 +1,18 @@
 import { useRef, useState, useEffect } from 'react';
 import Chess from 'chess.js';
+import bB from './media/bB.png';
+// import bK from './media/bK.png';
+// import bN from './media/bN.png';
+// import bP from './media/bP.png';
+// import bQ from './media/bQ.png';
+// import bR from './media/bR.png';
+
+// import wB from './media/wB.png';
+// import wK from './media/wK.png';
+// import wN from './media/wN.png';
+// import wP from './media/wP.png';
+// import wQ from './media/wQ.png';
+// import wR from './media/wR.png';
 
 import { Chessboard } from 'react-chessboard';
 
@@ -27,13 +40,36 @@ export default function ChessBoard({ moves }) {
 		currMove - 1 >= 0 && setCurrMove(currMove - 1);
 		// console.log(currMove);
 	};
+	const pieces = [
+		'wP',
+		'wN',
+		'wB',
+		'wR',
+		'wQ',
+		'wK',
+		'bP',
+		'bN',
+		'bB',
+		'bR',
+		'bQ',
+		'bK',
+	];
 
-	// useEffect(() => {
-	// 	console.log('useEffect called ' + currMove + ' ' + moves[currMove]);
-	// 	const didItLoad = game.load(moves[currMove]);
-	// 	console.log(didItLoad);
-	// }, [currMove]);
-
+	const customPieces = () => {
+		const returnPieces = {};
+		pieces.map((p) => {
+			returnPieces[p] = ({ squareWidth }) => (
+				<img
+					style={{ width: squareWidth, height: squareWidth }}
+					// src={`/media/${p}.png`}
+					src={`https://react-chessboard.com/media/${p}.png`}
+					alt={p}
+				/>
+			);
+			return null;
+		});
+		return returnPieces;
+	};
 	return (
 		<div>
 			<Chessboard
@@ -41,6 +77,14 @@ export default function ChessBoard({ moves }) {
 				arePiecesDraggable={false}
 				position={game.fen()}
 				animationDuration={200}
+				customBoardStyle={{
+					borderRadius: '4px',
+					boxShadow: '0 5px 15px rgba(0, 0, 0, 0.5)',
+				}}
+				customDarkSquareStyle={{ backgroundColor: '#A1B57D' }}
+				// customDarkSquareStyle={{ backgroundColor: '#D6E5FA' }}
+				customLightSquareStyle={{ backgroundColor: '#F7F7EE' }}
+				customPieces={customPieces()}
 				ref={chessboardRef}
 			/>
 
