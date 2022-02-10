ba6353a843b9efc6df237e2f41109a3bf57e8824
Ananya Gupta <ananyag2806@gmail.com>
2022-02-10 13:19:03 +0530

    implemented user registeration

--- routes/api/users.js
+++ routes/api/users.js
@@ -35,21 +35,11 @@ router.post(
 					.json({ errors: [{ msg: 'User already defined' }] });
 			}
 
-			const avatar = gravatar.url(email, {
-				s: '200',
-				r: 'pg',
-				d: 'mm',
-			});
-
 			user = new User({
 				name,
 				email,
-				avatar,
 				password,
 			});
-
-			const salt = await bcrypt.genSalt(10);
-			user.password = await bcrypt.hash(password, salt);
 
 			await user.save();
 
